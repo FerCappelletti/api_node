fb41be223616e206059a21e9301d3bf889afbbd3
FerCappelletti <fer_cappelletti@hotmail.com>
2023-05-09 12:13:52 -0300

    added docker & postgres db config to able data persistent

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,12 @@ const express = require('express')
 const router = express.Router()
 const servicesUsers = require('../services/users')
 
-router.get('/', async (req, res) => {
-  const users = await servicesUsers.getAllUsers(req, res)
-  res.json(users)
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await servicesUsers.getAllUsers(req, res)
+    return res.send({users})
+  } catch (error) {
+    next(error)
+  }
 })
 module.exports = router
